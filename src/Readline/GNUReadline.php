eyasudev/psysh

commit cd339488e1268b3b81651d4036a46d315f3cbfab

Better bracketed paste support detection.
As of PHP 7.4, the libedit-based readline implementation sometimes has history file support. When it does, PsySH will use the full "GNUReadline" implementation.

Unfortunately, we were also using that to determine whether we could allow bracketed paste. EditLine/libedit still hasn't added support for that, so we need to explicitly check whether we're using the real readline-based readline.

See #636

src/Readline/GNUReadline.php
@@ -14,7 +14,7 @@ namespace Psy\Readline;
 /**
  * A Readline interface implementation for GNU Readline.
  *
- * This is by far the coolest way to do it, but it doesn't work with new PHP.
+ * This is by far the coolest way to do it, if you can.
  *
  * Oh well.
  */
@@ -32,11 +32,24 @@ class GNUReadline implements Readline
      * decided it would be awesome to swap out GNU Readline for Libedit, but
      * they ended up shipping an incomplete implementation. So we've got this.
      *
+     * NOTE: As of PHP 7.4, PHP sometimes has history support in the Libedit
+     * wrapper, so that will use the GNUReadline implementation as well!
+     *
      * @return bool
      */
     public static function isSupported()
     {
-        return \function_exists('readline_list_history');
+        return \function_exists('readline') && \function_exists('readline_list_history');
+    }
+
+    /**
+     * Check whether this readline implementation supports bracketed paste.
+     *
+     * Currently, the GNU readline implementation does, but the libedit wrapper does not.
+     */
+    public static function supportsBracketedPaste()
+    {
+        return self::isSupported() && \stripos(\readline_info('library_version'), 'editline') === false;
     }
 
     /**

src/Configuration.php
@@ -818,7 +818,7 @@ class Configuration
     public function useBracketedPaste()
     {
         // For now, only the GNU readline implementation supports bracketed paste.
-        $supported = ($this->getReadlineClass() === Readline\GNUReadline::class);
+        $supported = ($this->getReadlineClass() === Readline\GNUReadline::class) && Readline\GNUReadline::supportsBracketedPaste();
 
         return $supported && $this->useBracketedPaste;
 

src/Readline/Libedit.php
@@ -19,6 +19,9 @@ use Psy\Util\Str;
  * This is largely the same as the Readline implementation, but it emulates
  * support for `readline_list_history` since PHP decided it was a good idea to
  * ship a fake Readline implementation that is missing history support.
+ *
+ * NOTE: As of PHP 7.4, PHP sometimes has history support in the Libedit
+ * wrapper, so it will use the GNUReadline implementation rather than this one.
  */
 class Libedit extends GNUReadline
 {

commit dc84473e1fa7e8400ba4dc40cb241031dda55ec8

Fix bracketed paste detection issue for some readline implementations (MinGWEditLine?) introduced in cd339488e126.
Fixes #687

src/Readline/GNUReadline.php
@@ -46,10 +46,12 @@ class GNUReadline implements Readline
      * Check whether this readline implementation supports bracketed paste.
      *
      * Currently, the GNU readline implementation does, but the libedit wrapper does not.
+     *
+     * @return bool
      */
     public static function supportsBracketedPaste(): bool
     {
-        return self::isSupported() && \stripos(\readline_info('library_version'), 'editline') === false;
+        return self::isSupported() && \stripos(\readline_info('library_version') ?: '', 'editline') === false;
     }
 
     /**